830c6057c9a6ae39fc4c2a917586fb233b477024
0xInfection <theinfecteddrake@gmail.com>
2020-01-23 21:10:01 +0530

    Attributed copyright notice to the rightful owner :)

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 #
 #       sipvicious/setup.py
 #
+#       Copyright (C) 2007-2020  Sandro Gauci <sandro@enablesecurity.com>
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
