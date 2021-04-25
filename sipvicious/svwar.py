df71b36669f2157360e79b929c12cd04257bdca3
Pinaki <theinfecteddrake@gmail.com>
2021-04-25 14:10:22 +0530

    Used fatal instead of error

--- sipvicious/svwar.py
+++ sipvicious/svwar.py
@@ -567,7 +567,7 @@ def main():
             try:
                 dictionary = open(options.dictionary, 'r', encoding='utf-8', errors='ignore')
             except IOError:
-                logging.error("could not open %s" % options.dictionary)
+                logging.fatal("could not open %s" % options.dictionary)
                 exit(1)
             if options.resume is not None:
                 lastextensionsrc = os.path.join(exportpath, 'lastextension.pkl')
