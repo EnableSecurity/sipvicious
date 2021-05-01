73c93dab65bb808e279b861b0b32e3a06c222934
Sandro Gauci <sandro@enablesecurity.com>
2021-05-01 05:45:52 +0200

    added example tests

---
+++ autotest/svmap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xu
+
+source common.sh
+
+do_test 30 "sipvicious_svmap wronghost"
+do_test 30 "sipvicious_svmap 1.2.3.4"
+do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 8888"
+do_test 40 "sipvicious_svmap demo.sipvicious.pro"
+
