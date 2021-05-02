fc329b18baca77628af7acf74d634ac323d2e990
Sandro Gauci <sandro@enablesecurity.com>
2021-05-02 07:03:39 +0200

    fixed svmap.sh

--- autotest/svmap.sh
+++ autotest/svmap.sh
@@ -6,5 +6,5 @@ source common.sh
 do_test 30 "sipvicious_svmap wronghost"
 do_test 30 "sipvicious_svmap 1.2.3.4"
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 8888"
-do_test 40 "sipvicious_svmap demo.sipvicious.pro"
+do_test 0 "sipvicious_svmap demo.sipvicious.pro"
 
