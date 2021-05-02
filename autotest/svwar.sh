739e7fa0f1c534effd625992856c0c3f8591ef31
Sandro Gauci <sandro@enablesecurity.com>
2021-05-02 07:04:16 +0200

    fixed mistake in svwar.sh

--- autotest/svwar.sh
+++ autotest/svwar.sh
@@ -7,5 +7,5 @@ do_test 30 "sipvicious_svwar wronghost"
 do_test 30 "sipvicious_svwar 1.2.3.4"
 do_test 30 "sipvicious_svwar demo.sipvicious.pro -p 8888"
 do_test 0 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -e 100-200"
-do_test 40 "sipvicious_svmap demo.sipvicious.pro -e 1000-1200"
+do_test 40 "sipvicious_svwar demo.sipvicious.pro -e 1000-1200"
 
