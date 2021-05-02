bee06500c4261663ab69aabc188b621f360c49cc
Sandro Gauci <sandro@enablesecurity.com>
2021-05-02 07:08:16 +0200

    fixed svcrack.sh

--- autotest/svcrack.sh
+++ autotest/svcrack.sh
@@ -3,9 +3,9 @@ set -xu
 
 source common.sh
 
-do_test 30 "sipvicious_svcrack wronghost"
-do_test 30 "sipvicious_svcrack 1.2.3.4"
-do_test 30 "sipvicious_svcrack demo.sipvicious.pro -p 8888"
-do_test 0 "sipvicious_svcrack demo.sipvicious.pro -u -r 100-200"
-do_test 40 "sipvicious_svcrack demo.sipvicious.pro -u -r 1400-1600"
+do_test 10 "sipvicious_svcrack nouser-syntaxerr"
+do_test 30 "sipvicious_svcrack 1.2.3.4 -u 100"
+do_test 30 "sipvicious_svcrack demo.sipvicious.pro -p 8888 -u 100"
+do_test 0 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 100-200"
+do_test 40 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 1400-1600"
 
