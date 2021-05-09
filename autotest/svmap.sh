2226f4c14a82549c5a0999f1ce8d1653304c88cc
0xInfection <theinfecteddrake@gmail.com>
2021-05-09 10:52:47 -0500

    added malformed ip detection

--- autotest/svmap.sh
+++ autotest/svmap.sh
@@ -3,6 +3,7 @@ set -xu
 
 source common.sh
 
+do_test 10 "sipvicious_svmap 1.1..1"
 do_test 30 "sipvicious_svmap wronghost"
 do_test 30 "sipvicious_svmap 1.2.3.4"
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 8888"
