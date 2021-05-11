9625cd7246b1d4dc75b818bdb1fb8d0623bd7ed2
Sandro Gauci <sandro@enablesecurity.com>
2021-05-11 12:19:38 +0200

    added more tests to svwar

--- autotest/svwar.sh
+++ autotest/svwar.sh
@@ -8,4 +8,9 @@ do_test 30 "sipvicious_svwar 1.2.3.4"
 do_test 30 "sipvicious_svwar demo.sipvicious.pro -p 8888"
 do_test 0 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -e 100-200"
 do_test 40 "sipvicious_svwar demo.sipvicious.pro -e 1000-1200"
-
+do_test 40 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -e 1000-1200"
+do_test 30 "sipvicious_svwar udp://demo.sipvicious.pro:8888 -e 1000-1200"
+do_test 20 "sipvicious_svwar -d test.txt demo.sipvicious.pro"
+echo 1100 > test2.txt
+do_test 40 "sipvicious_svwar -d test2.txt demo.sipvicious.pro"
+rm test2.txt
