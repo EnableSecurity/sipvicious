535c8e6db09acb95cd513a8621b2079e50d4dc49
Sandro Gauci <sandro@enablesecurity.com>
2021-05-11 12:10:06 +0200

    commented out ipv6 test on demo server

--- autotest/svmap.sh
+++ autotest/svmap.sh
@@ -13,5 +13,6 @@ do_test 0 "sipvicious_svmap 172.104.142.43" # demo.sipvicious.pro; this might ch
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 5060,8888"
 do_test 30 "sipvicious_svmap -6 ::1"
 do_test 30 "sipvicious_svmap -6 ::"
-do_test 0 "sipvicious_svmap -6 2a01:7e01::f03c:92ff:fecf:60a8" # demo.sipvicious.pro; this might change
+# commented out, GitHub actions does not appear to support ipv6
+# do_test 0 "sipvicious_svmap -6 2a01:7e01::f03c:92ff:fecf:60a8" # demo.sipvicious.pro; this might change
 do_test 30 "sipvicious_svmap 10.0.0.1-2 172.16.131.1 demo.sipvicious.pro/32 10.0.0.*"
