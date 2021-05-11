4d98dd4b904321674ec8c173f4c1bf314460b655
Sandro Gauci <sandro@enablesecurity.com>
2021-05-11 12:22:53 +0200

    fixed mistake in calling svcrack in test

--- autotest/svcrack.sh
+++ autotest/svcrack.sh
@@ -8,7 +8,7 @@ do_test 30 "sipvicious_svcrack 1.2.3.4 -u 100"
 do_test 30 "sipvicious_svcrack demo.sipvicious.pro -p 8888 -u 100"
 do_test 0 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 100-200"
 do_test 40 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 1400-1600"
-do_test 20 "sipvicious_crack demo.sipvicious.pro -d test.txt -u 1000"
+do_test 20 "sipvicious_svcrack demo.sipvicious.pro -d test.txt -u 1000"
 echo 1500 > test2.txt
-do_test 40 "sipvicious_crack demo.sipvicious.pro -d test2.txt -u 1000"
+do_test 40 "sipvicious_svcrack demo.sipvicious.pro -d test2.txt -u 1000"
 rm test2.txt
