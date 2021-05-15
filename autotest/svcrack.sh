59635cb3f00ea010e945f1036f3df0b891de248b
0xInfection <theinfecteddrake@gmail.com>
2021-05-15 19:55:59 +0530

    few more tests + fixed help message and exit code

--- autotest/svcrack.sh
+++ autotest/svcrack.sh
@@ -3,12 +3,27 @@ set -xu
 
 source common.sh
 
+# no -u supplied
 do_test 10 "sipvicious_svcrack nouser-syntaxerr"
+# invalid hostname syntax
+do_test 10 "sipvicious_svcrack udp://pew.pew"
+# again invalid syntax
+do_test 10 "sipvicious_svcrack pew.pew:5060"
+# multiple hosts (svcrack doesn't support it rn)
+do_test 10 "sipvicious_svcrack pew.pew:5060 ws://pew.pew:5060"
+# just the scheme with no URL (technically a valid URL)
+do_test 20 "sipvicious_svcrack blabla://"
+# non existent host
 do_test 30 "sipvicious_svcrack 1.2.3.4 -u 100"
+# invalid port on host
 do_test 30 "sipvicious_svcrack demo.sipvicious.pro -p 8888 -u 100"
+# valid user & hostname but wrong range
 do_test 0 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 100-200"
+# valid user & hostname with valid range
 do_test 40 "sipvicious_svcrack demo.sipvicious.pro -u 1000 -r 1400-1600"
+# non-existent dictionary file
 do_test 20 "sipvicious_svcrack demo.sipvicious.pro -d test.txt -u 1000"
+# valif dictionary file
 echo 1500 > test2.txt
 do_test 40 "sipvicious_svcrack demo.sipvicious.pro -d test2.txt -u 1000"
 rm test2.txt
--- sipvicious/svwar.py
+++ sipvicious/svwar.py
@@ -526,10 +526,10 @@ def main():
     parser.add_option('--force', dest="force", action="store_true",
         default=False, help="Force scan, ignoring initial sanity checks.")
     parser.add_option('--template', '-T', action="store", dest="template",
-        help="A format string which allows us to specify a template for the extensions" \
+        help="A format string which allows us to specify a template for the extensions. " \
             "example svwar.py -e 1-999 --template=\"123%#04i999\" would scan between 1230001999 to 1230999999\"")
     parser.add_option('--enabledefaults', '-D', action="store_true", dest="defaults",
-        default=False, help="Scan for default / typical extensions such as" \
+        default=False, help="Scan for default / typical extensions such as " \
             "1000,2000,3000 ... 1100, etc. This option is off by default." \
             "Use --enabledefaults to enable this functionality")
     parser.add_option('--maximumtime', action='store', dest='maximumtime', type="int",
@@ -596,7 +596,7 @@ def main():
             parser.error('Invalid protocol scheme: %s' % parsed.scheme, 20)
 
         if ':' not in parsed.netloc:
-            parser.error('You have to supply hosts in format of scheme://host:port when using newer convention.', 20)
+            parser.error('You have to supply hosts in format of scheme://host:port when using newer convention.', 10)
 
         if int(destport) != 5060:
             parser.error('You cannot supply additional -p when already including a port in URI. Please use only one.', 20)
