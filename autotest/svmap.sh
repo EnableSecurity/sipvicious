6af62c9d286664501294592ae27a3157730db450
0xInfection <theinfecteddrake@gmail.com>
2021-05-22 11:02:57 -0500

    final updates to test cases

--- autotest/svmap.sh
+++ autotest/svmap.sh
@@ -3,16 +3,40 @@ set -xu
 
 source common.sh
 
+# invalid ip format
 do_test 10 "sipvicious_svmap 1.1..1"
+# invalid host
 do_test 30 "sipvicious_svmap wronghost"
+# valid ip but not a sip talking host
 do_test 30 "sipvicious_svmap 1.2.3.4"
+# valid host but wrong port
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 8888"
+# valid host w/ wrong port range
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 8000-9000"
+# valid host w/ default port
 do_test 0 "sipvicious_svmap demo.sipvicious.pro"
+# valid ip w/ default port
 do_test 0 "sipvicious_svmap 172.104.142.43" # demo.sipvicious.pro; this might change
+# valid ip w/ different ports
 do_test 30 "sipvicious_svmap demo.sipvicious.pro -p 5060,8888"
+# ipv6 hosts
 do_test 30 "sipvicious_svmap -6 ::1"
 do_test 30 "sipvicious_svmap -6 ::"
 # commented out, GitHub actions does not appear to support ipv6
 # do_test 0 "sipvicious_svmap -6 2a01:7e01::f03c:92ff:fecf:60a8" # demo.sipvicious.pro; this might change
+# ip ranges & cidr ranges
 do_test 30 "sipvicious_svmap 10.0.0.1-2 172.16.131.1 demo.sipvicious.pro/32 10.0.0.*"
+# controlling packet rate + different method name + randomize
+do_test 30 "sipvicious_svmap 10.0.0.0/30 -t 3 -m INVITE --randomize"
+# ping a specific extension
+do_test 0 "sipvicious_svmap demo.sipvicious.pro -e 1000"
+# non-existent file
+do_test 20 "sipvicious_svmap -I nonexistent.txt"
+# IPs from file testing
+echo -e '1.1.1.1\n172.104.142.43' >> test.txt
+do_test 30 "sipvicious_svmap -I test.txt"
+rm test.txt
+# scan first few IPs
+do_test 30 "sipvicious_svmap 10.0.0.0/8 --first 2"
+# compact mode
+do_test 0 "sipvicious_svmap demo.sipvicious.pro -c"
--- autotest/svwar.sh
+++ autotest/svwar.sh
@@ -3,14 +3,27 @@ set -xu
 
 source common.sh
 
+# invalid host
 do_test 30 "sipvicious_svwar wronghost"
+# valid host not talking SIP
 do_test 30 "sipvicious_svwar 1.2.3.4"
+# valid host but wrong port
 do_test 30 "sipvicious_svwar demo.sipvicious.pro -p 8888"
+# valid url format but wrong extension range
 do_test 0 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -e 100-200"
+# valid host & valid extension range
 do_test 40 "sipvicious_svwar demo.sipvicious.pro -e 1000-1200"
+# valid url format with extension range
 do_test 40 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -e 1000-1200"
+# valid url format but wrong port w/ valid extension range
 do_test 30 "sipvicious_svwar udp://demo.sipvicious.pro:8888 -e 1000-1200"
+# non existent input dictionary files
 do_test 20 "sipvicious_svwar -d test.txt demo.sipvicious.pro"
+# valid dictionary file for extensions
 echo 1100 > test2.txt
 do_test 40 "sipvicious_svwar -d test2.txt demo.sipvicious.pro"
 rm test2.txt
+# enable defaults mode
+do_test 40 "sipvicious_svwar udp://demo.sipvicious.pro:5060 -D -m OPTIONS"
+# compact mode + packet rate
+do_test 40 "sipvicious_svwar demo.sipvicious.pro -e 1000-1005 -c -t 2"
